111b4b0a1392049e20c654b25ec27c2ea94544e7
Sharath S Rao <sharath.rao@zysk.tech>
2024-08-14 08:00:37 +0530

    feat: UI landing page

---
+++ pages/121.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="bg-amber-50 min-h-screen">
+    <!-- Header -->
+    <header class="bg-orange-700 text-white">
+      <div class="container mx-auto flex justify-between items-center">
+        <h1 class="text-xl font-bold">Hari Sarvottama</h1>
+        <img
+          src="https://vyasarajamatha.org/wp-content/uploads/2022/03/Hanuma3-.png"
+          alt="hanuma"
+        />
+        <div class="relative flex flex-col items-center">
+          <svg width="300" height="200">
+            <defs>
+              <path
+                id="curve"
+                d="M 150, 150 m -100, 0 a 100,100 0 1,1 200,0 a 100,100 0 1,1 -200,0"
+              />
+            </defs>
+            <text>
+              <textPath
+                xlink:href="#curve"
+                startOffset="25%"
+                text-anchor="middle"
+              >
+                Sri Moola Gopala Krishno Vijayathe
+              </textPath>
+            </text>
+          </svg>
+          <img
+            src="https://vyasarajamatha.org/wp-content/uploads/2022/03/circle-1-oif243d5q4sylp98lhxtjts6goczthixxotr90wtrk.png"
+            alt="Bheema"
+            class="w-24 h-24 rounded-full absolute"
+            style="top: 80px; left: 50%; transform: translateX(-50%)"
+          />
+        </div>
+        <img
+          src="https://vyasarajamatha.org/wp-content/uploads/2022/03/Garuda-Stone3.png"
+          alt="Bheema"
+        />
+        <h1 class="text-xl font-bold">Vaayu Jeevottama</h1>
+      </div>
+    </header>
+
+    <!-- Main content -->
+    <main class="container mx-auto mt-8 p-4">
+      <div class="bg-white rounded-lg shadow-lg p-6">
+        <!-- <div class="flex justify-between text-center">
+          <div class="w-1/3 p-4">
+            <h3 class="text-lg font-semibold mb-2 text-orange-700">
+              Sri Rajendratirthiya Poorvadi Matha
+            </h3>
+          </div>
+          <div class="w-1/3 p-4">
+            <h3 class="text-lg font-semibold mb-2 text-orange-700">
+              Sri Jagadguru Madhvacharya Moola Maha Samsthanam
+            </h3>
+          </div>
+          <div class="w-1/3 p-4">
+            <h3 class="text-lg font-semibold mb-2 text-orange-700">
+              Sri Vyasaraja Matha (Sosale)
+            </h3>
+          </div>
+        </div> -->
+
+        <h2 class="text-3xl font-bold text-center mb-4 text-orange-800">
+          Sri Tatwa Chandrika: Your Monthly Companion for Spiritual Growth and
+          Knowledge
+        </h2>
+
+        <!-- Main image -->
+        <div class="flex justify-center mb-6">
+          <div class="relative">
+            <img
+              src="https://anandsp1.wordpress.com/wp-content/uploads/2020/08/moola-gopalakrishna-devaru-20.jpg"
+              alt="Deity statue"
+              class="rounded-lg shadow-md max-w-2xl"
+            />
+            <div class="absolute top-2 right-2">
+              <!-- <img
+                src="require('@/assets/path-to-small-logo.png')"
+                alt="Small logo"
+                class="w-12 h-12"
+              /> -->
+            </div>
+          </div>
+        </div>
+
+        <!-- Mathas information -->
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LandingPage",
+};
+</script>
+
+<style scoped>
+svg text {
+  font-size: 18px;
+  font-weight: bold;
+  fill: white;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,176 @@
+<template>
+  <div class="bg-amber-50 min-h-screen">
+    <!-- Header -->
+    <header class="bg-orange-50">
+      <div class="container mx-auto flex justify-between items-center">
+        <h1 class="text-xl font-bold text-orange-700">Hari Sarvottama</h1>
+        <img
+          src="https://vyasarajamatha.org/wp-content/uploads/2022/03/Hanuma3-.png"
+          alt="hanuma"
+        />
+        <div class="relative flex flex-col items-center">
+          <svg width="300" height="200">
+            <defs>
+              <path
+                id="curve"
+                d="M 150, 150 m -100, 0 a 100,100 0 1,1 200,0 a 100,100 0 1,1 -200,0"
+              />
+            </defs>
+            <text>
+              <textPath
+                xlink:href="#curve"
+                startOffset="25%"
+                text-anchor="middle"
+              >
+                Sri Moola Gopala Krishno Vijayathe
+              </textPath>
+            </text>
+          </svg>
+          <img
+            src="https://vyasarajamatha.org/wp-content/uploads/2022/03/circle-1-oif243d5q4sylp98lhxtjts6goczthixxotr90wtrk.png"
+            alt="Bheema"
+            class="w-24 h-24 rounded-full absolute"
+            style="top: 80px; left: 50%; transform: translateX(-50%)"
+          />
+        </div>
+        <img
+          src="https://vyasarajamatha.org/wp-content/uploads/2022/03/Garuda-Stone3.png"
+          alt="Bheema"
+        />
+        <h1 class="text-xl font-bold text-orange-700">Vaayu Jeevottama</h1>
+      </div>
+    </header>
+
+    <!-- Institution Name -->
+    <section class="text-orange-800 text-center py-4 bg-orange-100">
+      <h2 class="text-2xl font-bold">
+        Sri Jagadguru Sriman Madhvacharya Moola Maha Samsthanam
+      </h2>
+      <h3 class="text-xl">Sri Rajendratirthiya Poorvadi Matha</h3>
+      <h3 class="font-bold text-2xl">Sri Vyasaraja Mutt (Sosale)</h3>
+    </section>
+
+    <!-- Main content -->
+    <main class="container mx-auto mt-6 p-6">
+      <div class="bg-white rounded-lg shadow-lg p-6 mt-4">
+        <h2 class="text-3xl font-bold text-center mb-4 text-orange-800">
+          Sri Tatwa Chandrika: Your Monthly Companion for Spiritual Growth and
+          Knowledge
+        </h2>
+
+        <!-- Main image -->
+        <div class="flex justify-center mb-6">
+          <div class="relative">
+            <img
+              src="https://anandsp1.wordpress.com/wp-content/uploads/2020/08/moola-gopalakrishna-devaru-20.jpg"
+              alt="Deity statue"
+              class="rounded-lg shadow-md max-w-2xl"
+            />
+          </div>
+        </div>
+
+        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mt-8">
+          <!-- Card 1 -->
+          <div class="rounded-lg shadow-md p-4 bg-orange-100">
+            <img
+              src="https://cdn.exoticindia.com/images/products/thumbnails/t800x600/books-2019/mzl307.jpg"
+              alt="Book 1"
+              class="w-full rounded-lg mb-4"
+            />
+            <p class="text-center text-orange-800 font-bold">January 2024</p>
+          </div>
+
+          <!-- Card 2 -->
+          <div class="bg-orange-100 rounded-lg shadow-md p-4">
+            <img
+              src="https://cdn.exoticindia.com/images/products/thumbnails/t800x600/books-2019/mzl307.jpg"
+              alt="Book 2"
+              class="w-full rounded-lg mb-4"
+            />
+            <p class="text-center text-orange-800 font-bold">February 2024</p>
+          </div>
+
+          <!-- Card 3 -->
+          <div class="bg-orange-100 rounded-lg shadow-md p-4">
+            <img
+              src="https://cdn.exoticindia.com/images/products/thumbnails/t800x600/books-2019/mzl307.jpg"
+              alt="Book 3"
+              class="w-full rounded-lg mb-4"
+            />
+            <p class="text-center text-orange-800 font-bold">March 2024</p>
+          </div>
+
+          <!-- Card 4 -->
+          <div class="bg-orange-100 rounded-lg shadow-md p-4">
+            <img
+              src="https://cdn.exoticindia.com/images/products/thumbnails/t800x600/books-2019/mzl307.jpg"
+              alt="Book 4"
+              class="w-full rounded-lg mb-4"
+            />
+            <p class="text-center text-orange-800 font-bold">April 2024</p>
+          </div>
+
+          <!-- Card 5 -->
+          <div class="bg-orange-100 rounded-lg shadow-md p-4">
+            <img
+              src="https://cdn.exoticindia.com/images/products/thumbnails/t800x600/books-2019/mzl307.jpg"
+              alt="Book 6"
+              class="w-full rounded-lg mb-4"
+            />
+            <p class="text-center text-orange-800 font-bold">May 2024</p>
+          </div>
+
+          <!-- Card 6 -->
+          <div class="bg-orange-100 rounded-lg shadow-md p-4">
+            <img
+              src="https://cdn.exoticindia.com/images/products/thumbnails/t800x600/books-2019/mzl307.jpg"
+              alt="Book 6"
+              class="w-full rounded-lg mb-4"
+            />
+            <p class="text-center text-orange-800 font-bold">June 2024</p>
+          </div>
+
+          <!-- Card 7 -->
+          <div class="bg-orange-100 rounded-lg shadow-md p-4">
+            <img
+              src="https://cdn.exoticindia.com/images/products/thumbnails/t800x600/books-2019/mzl307.jpg"
+              alt="Book 7"
+              class="w-full rounded-lg mb-4"
+            />
+            <p class="text-center text-orange-800 font-bold">July 2024</p>
+          </div>
+
+          <!-- Card 8 -->
+          <div class="bg-orange-100 rounded-lg shadow-md p-4">
+            <img
+              src="https://cdn.exoticindia.com/images/products/thumbnails/t800x600/books-2019/mzl307.jpg"
+              alt="Book 8"
+              class="w-full rounded-lg mb-4"
+            />
+            <p class="text-center text-orange-800 font-bold">August 2024</p>
+          </div>
+        </div>
+
+        <!-- Mathas information -->
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LandingPage",
+};
+</script>
+
+<style scoped>
+svg text {
+  font-size: 18px;
+  font-weight: bold;
+  fill: rgb(141, 12, 12);
+}
+
+header {
+  padding: 10px 0;
+}
+</style>
